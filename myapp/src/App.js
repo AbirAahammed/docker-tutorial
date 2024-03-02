c819eb9431acfa1d469239fb0f65bad3980d0254
AbirAahammed <43586864+AbirAahammed@users.noreply.github.com>
2024-03-02 07:43:22 +0000

    Added Docker stuff

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch("http://localhost:8080/users/", {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "your-api-key",
+        "X-RapidAPI-Host": "jokes-by-api-ninjas.p.rapidapi.com",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My App</h1>
     </div>
   );
 }
